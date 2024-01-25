8b5179da7dea8ee13379a6d88a1922f9fbb3cd7f
Jose Perez <55522084+aladuuu@users.noreply.github.com>
2024-01-25 19:59:39 -0300

    update

---
+++ webs/static/dilacon.cl/js/gm.js
@@ -0,0 +1,11 @@
+$('.map')
+      .gmap3({
+        center:[-33.4448006, -70.653319],
+        zoom:16,
+        mapTypeControl:false,
+        streetViewControl:false,
+        fullscreenControl:false,
+        zoomControl:false,
+        mapTypeId:"satellite",
+      })
+      ;     
