5da4f4f0a43d463ca816ab8224a3e7856ab045e2
joseperezm <55522084+aladuuu@users.noreply.github.com>
2024-07-01 04:44:38 -0400

    update

--- js/jjpm.js
+++ js/jjpm.js
@@ -206,3 +206,15 @@ document.addEventListener("DOMContentLoaded", function () {
   updateBackground();
   setInterval(updateBackground, updateInterval);
 });
+
+document.addEventListener("DOMContentLoaded", function() {
+    const dateInput = document.getElementById("date-input");
+    const today = new Date();
+    const year = today.getFullYear();
+    const month = String(today.getMonth() + 1).padStart(2, '0');
+    const day = String(today.getDate()).padStart(2, '0');
+    const formattedDate = `${year}-${month}-${day}`;
+  
+    dateInput.value = formattedDate;
+  });
+  
